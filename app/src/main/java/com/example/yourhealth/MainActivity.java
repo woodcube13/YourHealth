ba3477973511667080c82d16463a7bbebc2a6bee
JuYoung <wu7200@naver.com>
2019-05-16 09:45:34 +0900

    preset
    Preset for project

--- app/src/main/java/com/example/yourhealth/MainActivity.java
+++ app/src/main/java/com/example/yourhealth/MainActivity.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataType;
+
 public class MainActivity extends AppCompatActivity {
+
+    FitnessOptions fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent splashIntent = new Intent(this, splashActivity.class);
         startActivity(splashIntent);
+
+
     }
 }
