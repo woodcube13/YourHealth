ccb98b807be796c5f86011f5fbff4b0ea64cd3ec
JuYoung <wu7200@naver.com>
2019-05-23 20:16:43 +0900

    2019/05/23_20:16

--- app/src/main/java/com/example/yourhealth/GoogleSignInActivity.java
+++ app/src/main/java/com/example/yourhealth/GoogleSignInActivity.java
@@ -1,19 +1,35 @@
 package com.example.yourhealth;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import static com.example.yourhealth.R.id.main_activity_view;
 
 public class GoogleSignInActivity extends AppCompatActivity {
 
-    private static final int RC_READ_IN = 900;
+    private static final int RC_SIGN_IN = 900;
 
     private GoogleSignInClient googleSignInClient;
 
@@ -43,15 +59,41 @@ public class GoogleSignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent signInIntent = googleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, RC_READ_IN);
-            }
-            @Override
-            public void onActivityResult(int requestcode, int resultcode, Intent data){
-                super.onActivityResult(requestcode, requestcode, data);
-
-
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
 
+    }
+    @Override
+    public void onActivityResult(int requestcode, int resultcode, Intent data){
+        super.onActivityResult(requestcode, requestcode, data);
+
+        if(requestcode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            }catch (ApiException e){
+
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    Intent mainIntent = new Intent(GoogleSignInActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }else{
+                    Snackbar.make(findViewById(R.id.GoogleSignInActivity), "로그인에 실패하였습니다", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/yourhealth/MainActivity.java
+++ app/src/main/java/com/example/yourhealth/MainActivity.java
@@ -1,24 +1,111 @@
 package com.example.yourhealth;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    FitnessOptions fitnessOptions = FitnessOptions.builder()
-            .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
-            .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
-            .build();
+    public static final String TAG = "StepCounter";
+    private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
+    TextView stepview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stepview = (TextView) findViewById(R.id.textView);
 
+        Snackbar.make(findViewById(R.id.main_activity_view), "로그인에 성공했습니다", Snackbar.LENGTH_SHORT).show();
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .build();
+        if(!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)){
+            GoogleSignIn.requestPermissions(
+                    this,
+                    REQUEST_OAUTH_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        }else{
+            subscribe();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == REQUEST_OAUTH_REQUEST_CODE){
+                subscribe();
+            }
+        }
+    }
+
+    public void subscribe(){
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Log.i(TAG, "Success");
+                        }else{
+                            Log.i(TAG, "Fail", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void readData(){
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(new OnSuccessListener<DataSet>() {
+                    @Override
+                    public void onSuccess(DataSet dataSet) {
+                        long total = dataSet.isEmpty() ? 0 : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                        Log.i(TAG, "Total steps : " + total);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Fail get a step count", e);
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_read_data){
+            readData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
